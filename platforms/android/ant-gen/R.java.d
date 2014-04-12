01fbedcdcc5884bc0f9e99a32d7b065ebc854ad1
Allen Thomas Varghese <allentv4u@gmail.com>
2014-04-12 09:25:12 +0100

    Initial commit

---
+++ platforms/android/ant-gen/R.java.d
@@ -0,0 +1,14 @@
+F:\TCD\Accenture\GitRepo\platforms\android\ant-gen\com\accenture\codeathon\R.java \
+ : F:\TCD\Accenture\GitRepo\platforms\android\res\drawable\icon.png \
+F:\TCD\Accenture\GitRepo\platforms\android\res\drawable-hdpi\icon.png \
+F:\TCD\Accenture\GitRepo\platforms\android\res\drawable-ldpi\icon.png \
+F:\TCD\Accenture\GitRepo\platforms\android\res\drawable-mdpi\icon.png \
+F:\TCD\Accenture\GitRepo\platforms\android\res\drawable-xhdpi\icon.png \
+F:\TCD\Accenture\GitRepo\platforms\android\res\values\strings.xml \
+F:\TCD\Accenture\GitRepo\platforms\android\res\xml\config.xml \
+F:\TCD\Accenture\GitRepo\platforms\android\ant-build\res\drawable\icon.png \
+F:\TCD\Accenture\GitRepo\platforms\android\ant-build\res\drawable-hdpi\icon.png \
+F:\TCD\Accenture\GitRepo\platforms\android\ant-build\res\drawable-ldpi\icon.png \
+F:\TCD\Accenture\GitRepo\platforms\android\ant-build\res\drawable-mdpi\icon.png \
+F:\TCD\Accenture\GitRepo\platforms\android\ant-build\res\drawable-xhdpi\icon.png \
+F:\TCD\Accenture\GitRepo\platforms\android\ant-build\AndroidManifest.xml \
